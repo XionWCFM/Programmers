0f3d49919112abe61a1aa70945727fd8f04e6075
xion <101111364+XionWCFM@users.noreply.github.com>
2023-02-16 14:23:28 +0900

    [level 2] Title: 모음 사전, Time: 1.59 ms, Memory: 37.1 MB -BaekjoonHub

--- 프로그래머스/lv2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.js
@@ -1,27 +1,24 @@
-function generateWords(n, word, words) {
-  if (n === 0) {
-    words.push(word);
-    return;
-  }
-  for (let i = 0; i < 5; i++) {
-    generateWords(n - 1, word + ["A", "E", "I", "O", "U"][i], words);
-  }
-}
-
 function solution(word) {
-  let words = [];
-  for (let i = 1; i <= 5; i++) {
-    generateWords(i, "", words);
-  }
-
-  words.sort();
-
-  let count = 0;
-  for (let i = 0; i < words.length; i++) {
-    count++;
-    if (words[i] === word) {
-      return count;
+    let answer = []
+    
+    function maker(n,word,words) {
+        if(n === 0 ) {
+            words.push(word)
+            return
+        }    
+        for(let i=0; i<5; i++) {
+            maker(n-1, word + ['A','E','I','O','U'][i] , words)
+        }
+        
     }
-  }
+    
+    for(let i=1; i<=5 ; i++) {
+        maker(i, '', answer)
+    }
+    
+    answer.sort()
+    
+   for(let i=0 ; i<answer.length ;i++) {
+       if(answer[i] === word) return i+1
+   }
 }
-
